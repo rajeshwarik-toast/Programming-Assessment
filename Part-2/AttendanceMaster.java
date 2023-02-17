c2df57977a3d66d1943b5e4c645f00c30061aec9
rajeshwarik-toast <125260931+rajeshwarik-toast@users.noreply.github.com>
2023-02-17 11:45:15 +0530

    Update AttendanceMaster.java

--- Part-2/AttendanceMaster.java
+++ Part-2/AttendanceMaster.java
@@ -6,7 +6,7 @@ public class AttendanceMaster {
         Scanner sc = new Scanner(System.in);
         String noOfdays;
         for(Employee employee: employees ){
-            System.out.println("ENTER THE NO OF DAYS ");
+            System.out.println(">>>  ENTER THE NO OF DAYS OF " + employee.getEmpId() );
             noOfdays=sc.nextLine();
             while(!(noOfdays.matches("[0-9]+"))){
                 System.out.println("Enter valid number of days");
@@ -19,12 +19,13 @@ public class AttendanceMaster {
     public void showEligibleList() {
         Set<Map.Entry<Employee, Integer>> entrySet = dictionary.entrySet();
         for (Map.Entry<Employee, Integer> entry1 : entrySet) {
-        if(entry1.getValue()>10){
-                System.out.println("###   ELIGIBLE LIST   ###");
-                System.out.print(entry1.getKey().toString());
-                entry1.getKey().setAllowance();
-                System.out.println("    Attendance = " +entry1.getValue());
+            if(entry1.getValue()>10){
+                    System.out.println("###----------   ELIGIBLE LIST   ----------###");
+                    System.out.print(entry1.getKey().toString());
+                    entry1.getKey().setAllowance();
+                    System.out.println("    Attendance = " +entry1.getValue());
+                    System.out.println("--------------------------------------------------");
+            }
         }
-    }
     }
 }
