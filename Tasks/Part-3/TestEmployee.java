de343cbe83a21e4883be765f03e62d943de05523
rajeshwarik-toast <125260931+rajeshwarik-toast@users.noreply.github.com>
2023-03-02 14:04:57 +0530

    Update TestEmployee.java

--- Tasks/Part-3/TestEmployee.java
+++ Tasks/Part-3/TestEmployee.java
@@ -3,21 +3,23 @@ import java.util.Scanner;
 public class TestEmployee {
     static ArrayList<Employee> employee = new ArrayList<>();
     static AttendanceMaster attendanceMaster = new AttendanceMaster();
-    static MasterData masterData = new MasterData(employee);
     static SalCalculator salCalculator = new SalCalculator();
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         boolean flag=true;
+        boolean optionflag=false;
         System.out.printf("----------------------------------------------------------------------------------------------------------%n");
         System.out.printf("                                        PAYROLL MANAGEMENT                                                %n");
-        System.out.printf("----------------------------------------------------------------------------------------------------------%n");
+        System.out.printf("----------------------------------------------------------------------------------------------------------");
         while(flag) {
-            System.out.println("\nEMPLOYEE DETAILS\n    >>> 1. ADD EMPLOYEE\n    >>> 2. ADD ATTENDANCE\n    >>> " +
+            System.out.println("\n-> CHOOSE THE OPTION\n    >>> 1. ADD EMPLOYEE\n    >>> 2. ADD ATTENDANCE\n    >>> " +
                     "3. UPDATE ATTENDANCE \n    >>> 4. SORT \n    >>> 5. FILTER EMPLOYEE LIST \n    >>> 6. CALCULATE SALARY \n    >>> 7. DISPLAY DETAILS \n    >>> 8. EXIT \n  ENTER THE OPTION  :");
             String choice = sc.nextLine();
             String name, departmentOption, designationOption, salary;
             while (!(choice.matches("[1-8]"))) {
-                System.out.println(" ***   ENTER VALID OPTION   ***");
+                System.out.println(" ***   ENTER CHOICES FROM 1 TO 8   ***");
+                System.out.println("\n-> CHOOSE THE OPTION\n    >>> 1. ADD EMPLOYEE\n    >>> 2. ADD ATTENDANCE\n    >>> " +
+                        "3. UPDATE ATTENDANCE \n    >>> 4. SORT \n    >>> 5. FILTER EMPLOYEE LIST \n    >>> 6. CALCULATE SALARY \n    >>> 7. DISPLAY DETAILS \n    >>> 8. EXIT \n  ENTER THE OPTION  :");
                 choice = sc.nextLine();
             }
             switch (choice) {
@@ -32,7 +34,7 @@ public class TestEmployee {
                     departmentOption = sc.nextLine();
                     employee1.setDept(departmentOption);
                     System.out.println(">>>  ENTER EMPLOYEE DESIGNATION ");
-                    System.out.println("    1. Manager \n    2. Director \n    3. Software Developer \n    4. Quality Analyser");
+                    System.out.println("    1. Manager \n    2. Director \n    3. Software Developer \n    4. Accountant");
                     designationOption = sc.nextLine();
                     employee1.setDesg(designationOption);
                     System.out.println(">>>  ENTER EMPLOYEE SALARY ");
@@ -44,7 +46,7 @@ public class TestEmployee {
                 }
                 case "2": {
                     if(employee.isEmpty()){
-                        System.out.println("THE EMPLOYEE LIST IS EMPTY. \n >>> TO ADD THE EMPLOYEE DETAILS ENTER OPTION 1\n");
+                        System.out.println("***  THE EMPLOYEE LIST IS EMPTY !!! TO ADD THE EMPLOYEE DETAILS ENTER OPTION 1  ***");
                         break;
                     }
                     else {
@@ -56,118 +58,87 @@ public class TestEmployee {
                 case "3": {
                     if(!(attendanceMaster.employeeMap.isEmpty())){
                         attendanceMaster.updateAttendance();
+                        optionflag=false;
                         break;
                     }
-                    else {
-                        System.out.println("THE EMPLOYEE LIST MAYBE EMPTY OR ATTENDANCE IS NOT ADDED \n " +
-                                " KINDLY PRESS OPTION 1 ADD EMPLOYEE DETAILS OR PRESS OPTION 2 TO ADD ATTENDANCE");
+                    else if(employee.isEmpty()){
+                        System.out.println("***  THE EMPLOYEE LIST IS EMPTY !!! KINDLY PRESS OPTION 1 ADD EMPLOYEE DETAILS  ***");
+                        break;
+                    }
+                    else if(attendanceMaster.employeeMap.isEmpty()){
+                        System.out.println("***  ATTENDANCE HAS NOT BEEN ADDED !!! KINDLY PRESS OPTION 2 TO ADD ATTENDANCE  ***");
                         break;
                     }
                 }
                 case "4":{
                     if(!(employee.isEmpty())){
-                        System.out.println("##--------------------- SORTING ---------------------## \n    1. ID Sorting\n    2. Name Sorting\n    3. Department Sorting\n    4. Designation Sorting\n    5. Salary Sorting\n" +
+                        System.out.println("##--------------------- SORTING ---------------------## \n    1. SORT BY ID\n    2. SORT BY NAME\n    3. SORT BY DEPARTMENT\n    4. SORT BY DESIGNATION\n    5. SORT BY SALARY\n" +
                                 " ENTER THE OPTION ");
                         String option = sc.nextLine();
-                        System.out.println();
                         while (!(option.matches("[1-5]"))) {
-                            System.out.println(" >>> ENTER VALID OPTION\n");
+                            System.out.println("***  ENTER VALID OPTION  ***\n   1. SORT BY ID\n    2. SORT BY NAME\n    3. SORT BY DEPARTMENT\n    4. SORT BY DESIGNATION\n    5. SORT BY SALARY\n   ENTER THE OPTION ");
                             option = sc.nextLine();
                         }
-                        //masterData.quick_sort(employee, 0, employee.size() - 1, option);
-                        System.out.println(" TO SORT IN \n >>>> 1. ASCENDING ORDER\n >>>> 2. DESCENDING ORDER ");
+                        System.out.println(" TO SORT IN \n >>>> 1. ASCENDING ORDER\n >>>> 2. DESCENDING ORDER \n ENTER THE OPTION");
                         String sortoption = sc.nextLine();
                         while (!(sortoption.matches("[1-2]"))) {
-                            System.out.println(" >>> ENTER VALID OPTION\n");
+                            System.out.println("***  ENTER VALID OPTION  ***\n >>>> 1. ASCENDING ORDER\n >>>> 2. DESCENDING ORDER \n ENTER THE OPTION");
                             sortoption = sc.nextLine();
                         }
-                        //masterData.employeeSort(option,sortoption);
-
-                        switch (option){
-                            case "1": {
-                                if (sortoption.compareTo("1")==0) {
-                                    employee.sort((Employee e1, Employee e2)->e1.getEmpId()-e2.getEmpId());
-                                }
-                                if(sortoption.compareTo("2")==0){
-                                    employee.sort((Employee e1, Employee e2)->e2.getEmpId()-e1.getEmpId());
-                                }
-                                break;
-                            }
-                            case "2": {
-                                if (sortoption.compareTo("1")==0) {
-                                    employee.sort((Employee e1, Employee e2)->e1.getEmpName().compareTo(e2.getEmpName()));
-                                }
-                                if(sortoption.compareTo("2")==0){
-                                    employee.sort((Employee e1, Employee e2)->e2.getEmpName().compareTo(e1.getEmpName()));
-                                }
-                                break;
-                            }
-                            case "3": {
-                                if (sortoption.compareTo("1")==0) {
-                                    employee.sort((Employee e1, Employee e2)->e1.getDept().compareTo(e2.getDept()));
-                                }
-                                if(sortoption.compareTo("2")==0){
-                                    employee.sort((Employee e1, Employee e2)->e2.getDept().compareTo(e1.getDept()));
-                                }
-                                break;
-                            }
-                            case "4": {
-                                if (sortoption.compareTo("1")==0) {
-                                    employee.sort((Employee e1, Employee e2)->e1.getDesg().compareTo(e2.getDesg()));
-                                }
-                                if(sortoption.compareTo("2")==0){
-                                    employee.sort((Employee e1, Employee e2)->e2.getDesg().compareTo(e1.getDesg()));
-                                }
-                                break;
-                            }
-                            case "5": {
-                                if (sortoption.compareTo("1")==0) {
-                                    employee.sort((Employee e1, Employee e2)->e1.getSal().compareTo(e2.getSal()));
-                                }
-                                if(sortoption.compareTo("2")==0){
-                                    employee.sort((Employee e1, Employee e2)->e2.getSal().compareTo(e1.getSal()));
-                                }
-                                break;
-                            }
-                        }
-                        System.out.printf("| %-10s | %-8s | %-30s | %-20s | %-10s |%n", "ID", "NAME", "DEPARTMENT", "DESIGNATION", "SALARY");
-                        System.out.println("------------------------------------------------------------------------------------------------------------------------------");
-                        for (Employee employee1 : employee) {
-                            System.out.println(employee1.toString());
-                        }break;
+                        MasterData employeedata = new MasterData(employee);
+                        employeedata.employeeSort(option,sortoption);
+                        break;
                     }
                     else {
-                        System.out.println("THE EMPLOYEE LIST IS EMPTY!!\n KINDLY PRESS OPTION 1 ADD EMPLOYEE DETAILS");
+                        System.out.println("***  THE EMPLOYEE LIST IS EMPTY!!  KINDLY PRESS OPTION 1 ADD EMPLOYEE DETAILS  ***");
                         break;
                     }
                 }
                 case "5": {
                     if(!(attendanceMaster.employeeMap.isEmpty())){
                         attendanceMaster.filterEmployeeList();
+                        optionflag=true;
                         break;
                     }
-                    else {
-                        System.out.println("EITHER EMPLOYEE LIST IS EMPTY OR ATTENDANCE HAS NOT BEEN ADDED");
+                    else if(employee.isEmpty()){
+                        System.out.println("***  THE EMPLOYEE LIST IS EMPTY !!! KINDLY PRESS OPTION 1 ADD EMPLOYEE DETAILS  ***");
+                        break;
+                    }
+                    else if(attendanceMaster.employeeMap.isEmpty()){
+                        System.out.println("***  ATTENDANCE HAS NOT BEEN ADDED !!! KINDLY PRESS OPTION 2 TO ADD ATTENDANCE  ***");
                         break;
                     }
                 }
                 case "6":{
                     if(!(attendanceMaster.employeeMap.isEmpty())){
-                        salCalculator.calculateSalary(attendanceMaster.employeeMap);
+                        if(optionflag==true) {
+                            salCalculator.calculateSalary(attendanceMaster.employeeMap);
+                            break;
+                        }
+                        else{
+                            System.out.println("***  THE EMPLOYEE LIST IS NOT FILTERED!! KINDLY CHOOSE OPTION 5 TO FILTER THE EMPLOYEE LIST !!!");
+                            break;
+                        }
+                    }
+                    else if(employee.isEmpty()){
+                        System.out.println("***  THE EMPLOYEE LIST IS EMPTY !!! KINDLY PRESS OPTION 1 ADD EMPLOYEE DETAILS  ***");
                         break;
                     }
-                    else {
-                        System.out.println("FILTER THE EMPLOYEE LIST AND ENTER THE OPTION");
+                    else if(attendanceMaster.employeeMap.isEmpty()){
+                        System.out.println("***  ATTENDANCE HAS NOT BEEN ADDED !!! KINDLY PRESS OPTION 2 TO ADD ATTENDANCE  ***");
                         break;
                     }
                 }
                 case "7":{
-                    System.out.println("------------------------------------------------------------------------------------------------------------------------------");
-                    System.out.printf("| %-10s | %-8s | %-30s | %-20s | %-10s |%n", "ID", "NAME", "DEPARTMENT","DESIGNATION","SALARY");
-                    System.out.println("------------------------------------------------------------------------------------------------------------------------------");
-                    MasterData masterData = new MasterData(employee);
-                    System.out.println("------------------------------------------------------------------------------------------------------------------------------");
-                    break;
+                    if(!(employee.isEmpty())){
+                        MasterData masterData = new MasterData(employee);
+                        masterData.displayDetails();
+                        break;
+                    }
+                    else{
+                        System.out.println("***  THE EMPLOYEE LIST IS EMPTY !!! KINDLY PRESS OPTION 1 ADD EMPLOYEE DETAILS  ***");
+                        break;
+                    }
                 }
                 case "8" : {
                     flag=false;
