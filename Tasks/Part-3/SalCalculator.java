8e39d51eb8bd59c0e7ef16fbba1c9ab159d3db34
rajeshwarik-toast <125260931+rajeshwarik-toast@users.noreply.github.com>
2023-03-02 14:05:21 +0530

    Update SalCalculator.java

--- Tasks/Part-3/SalCalculator.java
+++ Tasks/Part-3/SalCalculator.java
@@ -4,19 +4,15 @@ public class SalCalculator {
     public void calculateSalary(LinkedHashMap<Employee,Integer> employeeHashMap){
         double pf,gross,net;
         Set<Employee> entrySet = employeeHashMap.keySet();
+        System.out.printf("| %-10s | %-8s | %-30s | %-20s | %-10s | %-10s | %-10s | %-10s | %-10s |%n", "ID", "NAME", "DEPARTMENT","DESIGNATION","SALARY","ALLOWANCE","GROSS","PF","NET");
+        System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
         for (Employee employee : entrySet) {
             pf = (10 * employee.getSal())/100;
             gross = employee.getSal()+employee.initialSalary();
             net = gross - pf;
-            System.out.printf("| %-10s | %-8s | %-20s | %-20s | %-10s |%n", "ID", "NAME", "DEPARTMENT","DESIGNATION","SALARY");
-            System.out.println("------------------------------------------------------------------------------------------------------------------------------");
-            for(int i=0;i<employeeHashMap.size();i++) {
-                System.out.println(employee.toString());
-            }
-            System.out.println("    ALLOWANCE " + String.format("%.2f", employee.initialSalary()));
-            System.out.println("    GROSS     " + String.format("%.2f",gross));
-            System.out.println("    PF        " + String.format("%.2f",pf));
-            System.out.println("    NET       " + String.format("%.2f",net));
+            System.out.print(employee.toString());
+            System.out.printf(" %-10s | %-10s | %-10s | %-10s |",String.format("%.2f", employee.initialSalary()),String.format("%.2f",gross),String.format("%.2f",pf),String.format("%.2f",net));
+            System.out.println();
         }
     }
 }
