92ce1d51645d493e85728a412f79885a77037388
rajeshwarik-toast <125260931+rajeshwarik-toast@users.noreply.github.com>
2023-03-05 21:30:24 +0530

    Update AttendanceMaster.java

--- Tasks/Part-3/AttendanceMaster.java
+++ Tasks/Part-3/AttendanceMaster.java
@@ -6,14 +6,16 @@ public class AttendanceMaster {
         String noOfdays;
         for (Employee employee : employees) {
             if(!(employeeMap.containsKey(employee))){
-                System.out.println(">>>  ENTER THE NO OF DAYS OF " + employee.getEmpId());
-                noOfdays = sc.nextLine();
-                while (!(noOfdays.matches("[0-9]+"))) {
-                    System.out.println("*** ENTER THE VALID NO OF DAYS ***");
+                if(!(employees.contains(employee))) {
+                    System.out.println(">>>  ENTER THE NO OF DAYS OF " + employee.getEmpId());
                     noOfdays = sc.nextLine();
+                    while (!(noOfdays.matches("[0-9]+"))) {
+                        System.out.println("*** ENTER THE VALID NO OF DAYS ***");
+                        noOfdays = sc.nextLine();
+                    }
+                    int attendance = Integer.parseInt(noOfdays);
+                    employeeMap.put(employee, attendance);
                 }
-                int attendance = Integer.parseInt(noOfdays);
-                employeeMap.put(employee, attendance);
             }
         }
     }
@@ -24,7 +26,7 @@ public class AttendanceMaster {
         System.out.println("----------------------------------------------------------------------------------------------------------");
         for (Map.Entry<Employee, Integer> entry1 : entrySet) {
             if (entry1.getValue() > 10) {
-                System.out.print(entry1.getKey().toString());
+                System.out.print(entry1.getKey());
                 System.out.printf(" %-10s |",entry1.getValue());
                 System.out.println();
             }
@@ -32,44 +34,37 @@ public class AttendanceMaster {
     }
     public void updateAttendance() {
         String employee_id, attendance;
-        Employee employee = new Employee();
+        ArrayList<Integer> availableIdList = new ArrayList<>();
         System.out.println(">>>  ENTER THE ID OF THE EMPLOYEE TO UPDATE ATTENDANCE");
         employee_id = sc.nextLine();
-        ArrayList<Integer> availableIdList = new ArrayList<>();
-        for (Map.Entry entry : employeeMap.entrySet()) {
-            Employee e = (Employee) entry.getKey();
-            availableIdList.add(e.getEmpId());
-        }
-        while (!((employee_id.matches("[0-9]+")) && availableIdList.contains(Integer.parseInt(employee_id)))) {
-            System.out.println("*** EITHER ID IS NOT IN THE ATTENDANCE LIST OR NOT IN CORRECT FORMAT ***\n");
+        while ((!(employee_id.matches("[0-9]+")))) {
+            System.out.println("*** EITHER ID IS NOT IN THE EMPLOYEE LIST OR NOT IN CORRECT FORMAT ***\n");
             employee_id = sc.nextLine();
         }
-        for (Employee employees : TestEmployee.employee) {
-            if (employees.getEmpId() == Integer.valueOf(employee_id)) {
-                employee = employees;
-            }
+        if (Employee.counter < Integer.valueOf(employee_id)) {
+            System.out.println("*** EMPLOYEE ID NOT FOUND ***");
         }
-        System.out.println(">>>  ENTER THE ATTENDANCE TO BE UPDATED");
-        attendance = sc.nextLine();
-        while (!(attendance.matches("[0-9]+"))) {
-            System.out.println("*** ENTER VALID ATTENDANCE ***\n");
+        else {
+            System.out.println(">>>  ENTER THE ATTENDANCE TO BE UPDATED");
             attendance = sc.nextLine();
-        }
-        int updateAttendance = Integer.parseInt(attendance);
-        employeeMap.replace(employee, updateAttendance);
-        System.out.println("\n-------------------------------------------->>> UPDATED LIST <<<------------------------------------------");
-        System.out.printf("| %-8s | %-15s | %-30s | %-20s | %-10s | %-10s |%n", "ID", "NAME", "DEPARTMENT","DESIGNATION","SALARY","ATTENDANCE");
-        System.out.println("----------------------------------------------------------------------------------------------------------");
-        for (Map.Entry entry : employeeMap.entrySet()) {
-            System.out.print(entry.getKey());
-            System.out.printf(" %-10s |",entry.getValue());
-            System.out.println();
+            while (!(attendance.matches("[0-9]+"))) {
+                System.out.println("*** ENTER VALID ATTENDANCE ***\n");
+                attendance = sc.nextLine();
+            }
+            int updateAttendance = Integer.parseInt(attendance);
+            for(Employee employee1:TestEmployee.employee) {
+                if (employee1.getEmpId() == Integer.parseInt(employee_id)) {
+                    employeeMap.put(employee1, updateAttendance);
+                }
+            }
+            System.out.println("UPDATED SUCCESSFULLY !!!");
         }
     }
     public void filterEmployeeList() {
         ArrayList<Employee> employeeArrayList = new ArrayList<>(employeeMap.keySet());
         for(Employee employee : employeeArrayList){
             if(this.employeeMap.get(employee)<10){
+                employeeArrayList.add(employee);
                 employeeMap.remove(employee);
             }
         }
