e8dd4b66495018ac7c2429371822678d505ed2f5
rajeshwarik-toast <125260931+rajeshwarik-toast@users.noreply.github.com>
2023-03-06 11:19:17 +0530

    Update AttendanceMaster.java

--- Tasks/Part-3/AttendanceMaster.java
+++ Tasks/Part-3/AttendanceMaster.java
@@ -1,78 +1,146 @@
-import java.util.*;
-public class AttendanceMaster {
-    LinkedHashMap<Employee, Integer> employeeMap = new LinkedHashMap<>();
-    private ArrayList<Employee> filteredlist = new ArrayList<>();
-    Scanner sc = new Scanner(System.in);
-    public void getAttendance(ArrayList<Employee> employees) {
-        String noOfdays;
-        for (Employee employee : employees) {
-            if(!(employeeMap.containsKey(employee))){
-                if(!(filteredlist.contains(employee))) {
-                    System.out.println(">>>  ENTER THE NO OF DAYS OF " + employee.getEmpId());
-                    noOfdays = sc.nextLine();
-                    while (!(noOfdays.matches("[0-9]+"))) {
-                        System.out.println("*** ENTER THE VALID NO OF DAYS ***");
-                        noOfdays = sc.nextLine();
+import java.util.ArrayList;
+import java.util.Scanner;
+public class TestEmployee {
+    static ArrayList<Employee> employee = new ArrayList<>();
+    static AttendanceMaster attendanceMaster = new AttendanceMaster();
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        boolean flag=true; //for main menu
+        boolean optionflag=false; //for filtering the employee list
+        System.out.printf("-----------------------------------------------------------------------------------------------------------%n");
+        System.out.printf("                                     PAYROLL MANAGEMENT SYSTEM                                             %n");
+        System.out.printf("-----------------------------------------------------------------------------------------------------------");
+        while(flag) {
+            System.out.println("\n-> CHOOSE AN OPTION\n    >>> 1. ADD EMPLOYEE\n    >>> 2. ADD ATTENDANCE\n    >>> " +
+                    "3. UPDATE ATTENDANCE \n    >>> 4. SORT \n    >>> 5. FILTER EMPLOYEE LIST \n    >>> 6. CALCULATE SALARY \n    >>> 7. DISPLAY DETAILS \n    >>> 8. EXIT \n  ENTER THE OPTION  :");
+            String choice = sc.nextLine();
+            String name, departmentOption, designationOption, salary;
+            while (!(choice.matches("[1-8]"))) {
+                System.out.println(" ***   ENTER CHOICES FROM 1 TO 8   ***");
+                System.out.println("\n-> CHOOSE THE OPTION\n    >>> 1. ADD EMPLOYEE\n    >>> 2. ADD ATTENDANCE\n    >>> " +
+                        "3. UPDATE ATTENDANCE \n    >>> 4. SORT \n    >>> 5. FILTER EMPLOYEE LIST \n    >>> 6. CALCULATE SALARY \n    >>> 7. DISPLAY DETAILS \n    >>> 8. EXIT \n  ENTER THE OPTION  :");
+                choice = sc.nextLine();
+            }
+            switch (choice) {
+                case "1": {
+                    System.out.println("\n-------------------- ENTER EMPLOYEE DETAILS --------------------");
+                    System.out.println(">>>  EMPLOYEE NAME :  ");
+                    name = sc.nextLine();
+                    Employee employee1 = new Employee();
+                    employee1.setEmpName(name);
+                    System.out.println(">>>  ENTER EMPLOYEE DEPARTMENT  ");
+                    System.out.println("    1. Customer Service \n    2. Marketing & Sales \n    3. Resource & Development \n    4. IT");
+                    departmentOption = sc.nextLine();
+                    employee1.setDept(departmentOption);
+                    System.out.println(">>>  ENTER EMPLOYEE DESIGNATION ");
+                    System.out.println("     1. Manager \n    2. Director \n    3. Software Developer \n    4. Accountant");
+                    designationOption = sc.nextLine();
+                    employee1.setDesg(designationOption);
+                    System.out.println(">>>  ENTER EMPLOYEE SALARY ");
+                    salary = sc.nextLine();
+                    employee1.setSal(salary);
+                    employee1.setAllowance();
+                    employee.add(employee1);
+                    break;
+                }
+                case "2": {
+                    if(employee.isEmpty()){  //condition to check whether the employee list is empty
+                        System.out.println("***  THE EMPLOYEE LIST IS EMPTY !!! TO ADD THE EMPLOYEE DETAILS ENTER OPTION 1  ***");
+                        break;
                     }
-                    int attendance = Integer.parseInt(noOfdays);
-                    employeeMap.put(employee, attendance);
+                    else {  //if not empty
+                        attendanceMaster.getAttendance(employee);
+                        attendanceMaster.showEligibleList();
+                        break;
+                    }
+                }
+                case "3": {
+                    AttendanceMaster attendanceMaster = new AttendanceMaster();
+                    if(!(employee.isEmpty())){ //employee list is not empty
+                        attendanceMaster.updateAttendance();
+                        optionflag=false;
+                        break;
+                    }
+                    else {   //employee is empty
+                        System.out.println("***  THE EMPLOYEE LIST IS EMPTY !!! KINDLY PRESS OPTION 1 ADD EMPLOYEE DETAILS  ***");
+                        break;
+                    }
+                }
+                case "4":{
+                    if(!(employee.isEmpty())){  //employee is not empty
+                        System.out.println("##--------------------- SORTING ---------------------## \n    1. SORT BY ID\n    2. SORT BY NAME\n    3. SORT BY DEPARTMENT\n    4. SORT BY DESIGNATION\n    5. SORT BY SALARY\n" +
+                                " ENTER THE OPTION ");
+                        String option = sc.nextLine();
+                        while (!(option.matches("[1-5]"))) {
+                            System.out.println("***  ENTER VALID OPTION  ***\n   1. SORT BY ID\n    2. SORT BY NAME\n    3. SORT BY DEPARTMENT\n    4. SORT BY DESIGNATION\n    5. SORT BY SALARY\n   ENTER THE OPTION ");
+                            option = sc.nextLine();
+                        }
+                        System.out.println(" TO SORT IN \n >>>> 1. ASCENDING ORDER\n >>>> 2. DESCENDING ORDER \n ENTER THE OPTION");
+                        String sortoption = sc.nextLine();
+                        while (!(sortoption.matches("[1-2]"))) {    //ascending or descending
+                            System.out.println("***  ENTER VALID OPTION  ***\n >>>> 1. ASCENDING ORDER\n >>>> 2. DESCENDING ORDER \n ENTER THE OPTION");
+                            sortoption = sc.nextLine();
+                        }
+                        MasterData employeedata = new MasterData(employee);
+                        employeedata.employeeSort(option,sortoption);
+                        break;
+                    }
+                    else {  //employee is empty
+                        System.out.println("***  THE EMPLOYEE LIST IS EMPTY!!  KINDLY PRESS OPTION 1 ADD EMPLOYEE DETAILS  ***");
+                        break;
+                    }
+                }
+                case "5": {
+                    if(!(attendanceMaster.employeeMap.isEmpty())){  //employee and attendance is not empty
+                        attendanceMaster.filterEmployeeList();
+                        optionflag=true;
+                        break;
+                    }
+                    else if(employee.isEmpty()){    //employee is empty
+                        System.out.println("***  THE EMPLOYEE LIST IS EMPTY !!! KINDLY PRESS OPTION 1 ADD EMPLOYEE DETAILS  ***");
+                        break;
+                    }
+                    else if(attendanceMaster.employeeMap.isEmpty()){
+                        System.out.println("***  ATTENDANCE HAS NOT BEEN ADDED !!! KINDLY PRESS OPTION 2 TO ADD ATTENDANCE  ***");
+                        break;
+                    }
+                }
+                case "6":{
+                    SalCalculator salCalculator = new SalCalculator();
+                    if(!(attendanceMaster.employeeMap.isEmpty())){
+                        if(optionflag==true) {
+                            salCalculator.calculateSalary(attendanceMaster.employeeMap);
+                            break;
+                        }
+                        else{
+                            System.out.println("***  THE EMPLOYEE LIST IS NOT FILTERED!! KINDLY CHOOSE OPTION 5 TO FILTER THE EMPLOYEE LIST !!!");
+                            break;
+                        }
+                    }
+                    else if(employee.isEmpty()){
+                        System.out.println("***  THE EMPLOYEE LIST IS EMPTY !!! KINDLY PRESS OPTION 1 ADD EMPLOYEE DETAILS  ***");
+                        break;
+                    }
+                    else if(attendanceMaster.employeeMap.isEmpty()){
+                        System.out.println("***  NO EMPLOYEE FOUND TO CALCULATE SALARY !!!  ***");
+                        break;
+                    }
+                }
+                case "7":{
+                    if(!(employee.isEmpty())){
+                        MasterData masterData = new MasterData(employee);
+                        masterData.displayDetails();
+                        break;
+                    }
+                    else{
+                        System.out.println("***  THE EMPLOYEE LIST IS EMPTY !!! KINDLY PRESS OPTION 1 ADD EMPLOYEE DETAILS  ***");
+                        break;
+                    }
+                }
+                case "8" : {
+                    flag=false;
                 }
             }
-        }
-    }
-    public void showEligibleList() {
-        Set<Map.Entry<Employee, Integer>> entrySet = employeeMap.entrySet();
-        System.out.println("\n------------------------------------------>>> ELIGIBLE LIST <<<------------------------------------------");
-        System.out.printf("| %-8s | %-15s | %-30s | %-20s | %-10s | %-10s |%n", "ID", "NAME", "DEPARTMENT","DESIGNATION","SALARY","ATTENDANCE");
-        System.out.println("----------------------------------------------------------------------------------------------------------");
-        for (Map.Entry<Employee, Integer> entry1 : entrySet) {
-            if (entry1.getValue() >= 10) {
-                System.out.print(entry1.getKey());
-                System.out.printf(" %-10s |",entry1.getValue());
-                System.out.println();
-            }
-        }
-    }
-    public void updateAttendance() {
-        String employee_id, attendance;
-        System.out.println(">>>  ENTER THE ID OF THE EMPLOYEE TO UPDATE ATTENDANCE");
-        employee_id = sc.nextLine();
-        while ((!(employee_id.matches("[0-9]+")))) {
-            System.out.println("*** EITHER ID IS NOT IN THE EMPLOYEE LIST OR NOT IN CORRECT FORMAT ***\n");
-            employee_id = sc.nextLine();
-        }
-        if (Employee.counter < Integer.valueOf(employee_id)) {
-            System.out.println("*** EMPLOYEE ID NOT FOUND ***");
-        }
-        else {
-            System.out.println(">>>  ENTER THE ATTENDANCE TO BE UPDATED");
-            attendance = sc.nextLine();
-            while (!(attendance.matches("[0-9]+"))) {
-                System.out.println("*** ENTER VALID ATTENDANCE ***\n");
-                attendance = sc.nextLine();
-            }
-            int updateAttendance = Integer.parseInt(attendance);
-            for(Employee employee1:TestEmployee.employee) {
-                if (employee1.getEmpId() == Integer.parseInt(employee_id)) {
-                    employeeMap.put(employee1, updateAttendance);
-                }
-            }
-            System.out.println("UPDATED SUCCESSFULLY !!!");
-        }
-    }
-    public void filterEmployeeList() {
-        ArrayList<Employee> employeeArrayList = new ArrayList<>(employeeMap.keySet());
-        for(Employee employee : employeeArrayList){
-            if(this.employeeMap.get(employee)<10){
-                employeeArrayList.add(employee);
-                employeeMap.remove(employee);
-            }
-        }
-        if(!(employeeMap.isEmpty())) {
-            showEligibleList();
-        }
-        else{
-            System.out.println("***  ELIGIBLE LIST IS EMPTY  ***");
         }
     }
 }
