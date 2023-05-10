ed59566c48127eb42bb86dbc92181349ebdff064
rajeshwarik-toast <rajeshwari.k@toasttab.com>
2023-05-10 17:37:18 +0530

    Added

---
+++ MVC Projects/WebApplication2/WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication2.Models;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Authorization;
+using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Finalpage()
+        {
+            return View();
+        }
+
+        public ActionResult Login(string username, string password)
+        {
+            using (SqlConnection connection = new SqlConnection("Data Source=xc-db-int2019.cweclpjrmoc6.us-east-1.rds.amazonaws.com,1357;Initial Catalog=XTRACHEF_Freshers;User ID=xcdbAppUser;Password=GJGwAWjB7C2tSSad65NO;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("SELECT * FROM XTRACHEF_EMPLOYEE WHERE EMPLOYEE_USERNAME=@username AND EMPLOYEE_PASSWORD=@password", connection);
+                command.Parameters.AddWithValue("@username", username);
+                command.Parameters.AddWithValue("@password", password);
+                command.ExecuteReader();
+                if (true)
+                {
+                    // Login successful
+                    return View("Finalpage");
+                }
+                else
+                {
+                    // Login failed
+                    ModelState.AddModelError("", "Invalid username or password");
+                    return View("Index");
+                }
+            }
+        }
+    }
+}
---
+++ MVC Projects/WebApplication2/WebApplication2/Models/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Please enter your username.")]
+        //[Name(ErrorMessage = "Please enter a valid username.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Please enter your password.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+    }
+
+}
